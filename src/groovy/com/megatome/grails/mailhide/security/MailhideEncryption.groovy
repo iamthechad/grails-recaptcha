a8e9c724c0aef0ee078d53aa6b5fed67510e634c
Chad Johnston <cjohnston@megatome.com>
2010-07-15 13:03:49 +0000

    Encryption changs

--- src/groovy/com/megatome/grails/mailhide/security/MailhideEncryption.groovy
+++ src/groovy/com/megatome/grails/mailhide/security/MailhideEncryption.groovy
@@ -10,42 +10,15 @@ class MailhideEncryption {
     cipher.doFinal(string.getBytes())
   }
 
-  static decode = { str ->
-    "bar"
-  }
-
-  private static byte[] hexStringToByteArray(String encoded) {
-    byte[] result = new byte[encoded.length()/2];
-    char[] enc = encoded.toCharArray();
-    for (int i = 0; i < enc.length; i += 2) {
-        StringBuilder curr = new StringBuilder(2);
-        curr.append(enc[i]).append(enc[i + 1]);
-        result[i/2] = Integer.parseInt(curr.toString(), 16) as byte;
-    }
-    return result;
-  }
-
   private static setupCipher(mode, key) {
-        Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
+        def cipher = Cipher.getInstance("AES/CBC/NoPadding")
 
-        // setup key
-        //byte[] keyBytes = new byte[16];
-        //byte[] b = password.getBytes("UTF-8");
-        //println b
-        //println hexStringToByteArray(password)
-        //println b.encodeAsHex()
-        //int len = b.length;
-        //if (len > keyBytes.length)
-        //      len = keyBytes.length;
-        //System.arraycopy(b, 0, keyBytes, 0, len);
-        SecretKeySpec keySpec = new SecretKeySpec(key, "AES");
+        def keyBytes = key.decodeHex()
+        def keySpec = new SecretKeySpec(keyBytes, "AES")
 
-        byte[] iv = new byte[16]
-        iv.each {
-          it = 0X00
-        }
-        IvParameterSpec ivSpec = new IvParameterSpec(iv);
-        cipher.init(mode, keySpec, ivSpec);
-        return cipher
+        byte[] iv = ([0] * 16) as byte[]
+        def ivSpec = new IvParameterSpec(iv)
+        cipher.init(mode, keySpec, ivSpec)
+        cipher
     }
 }
--- test/unit/com/megatome/grails/mailhide/security/MailhideEncryptionTest.groovy
+++ test/unit/com/megatome/grails/mailhide/security/MailhideEncryptionTest.groovy
@@ -4,29 +4,32 @@ import com.megatome.grails.mailhide.util.StringUtils
 import org.codehaus.groovy.grails.plugins.codecs.HexCodec
 
 class MailhideEncryptionTest extends GroovyTestCase {
-  private static String PRIVATE_KEY = "deadbeefdeadbeefdeadbeefdeadbeef"
-  private static byte[] encrypted = [0Xc0, 0X11, 0Xbb, 0X9c, 0Xe8, 0X27, 0Xb4, 0Xaa, 0X96, 0X78, 0X3a, 0X45, 0Xf6, 0Xe7, 0X15, 0X35]
-  static def expected = 0Xc011bb9ce827b4aa96783a45f6e71535 as String
-  
-  void testRoundtrip() {
-        MailhideEncryption me = new MailhideEncryption()
-        def original = "x@example.com"
-        def encoded = me.encrypt(StringUtils.padString(original), HexCodec.decode(PRIVATE_KEY))
-        assert null != encoded
-        println HexCodec.encode(encrypted)
-        println HexCodec.decode(PRIVATE_KEY)
-        println hexdump(encrypted)
-        println hexdump(encoded)
-        assert encrypted == encoded        
-        //def decoded = encoded.decodeAES()
+  static final String PRIVATE_KEY = "deadbeefdeadbeefdeadbeefdeadbeef"
 
-        //assert original != encoded
-        //assert encoded != decoded
-        //assert original == decoded
+  static final String emailAddress1 = "x@example.com"
+  static final byte[] encrypted1 = [0Xc0, 0X11, 0Xbb, 0X9c, 0Xe8, 0X27, 0Xb4, 0Xaa, 0X96, 0X78, 0X3a, 0X45, 0Xf6, 0Xe7, 0X15, 0X35]
+
+  static final String emailAddress2 = "johndoe@example.com"
+  static final byte[] encrypted2 = [0Xc2, 0X15, 0X88, 0Xaa, 0X4d, 0X2b, 0Xe2, 0Xea, 0Xd9, 0Xfb, 0X74, 0Xbb, 0Xcb, 0Xbb, 0X92, 0X71,
+  0Xe0, 0Xbd, 0Xfc, 0X40, 0X9d, 0Xde, 0X1a, 0X40, 0X1b, 0X2e, 0Xf5, 0X13, 0X6a, 0X34, 0X1e, 0X92]
+
+  def data = [:]
+
+  void setUp() {
+    super.setUp()
+    String.metaClass.decodeHex = {
+      HexCodec.decode(delegate)
     }
+    data[emailAddress1] = encrypted1
+    data[emailAddress2] = encrypted2
+  }
 
-  private def hexdump(bytes) {
-    def h = new java.math.BigInteger(1, bytes).toString(16)
-    return bytes.size() ? h.length() & 1 ? "0" + h : h : ""
+  void testRoundtrip() {
+    MailhideEncryption me = new MailhideEncryption()
+    data.each {k, v->
+      def encoded = me.encrypt(StringUtils.padString(k), PRIVATE_KEY)
+      assert null != encoded
+      assert v == encoded
+    }
   }
 }
