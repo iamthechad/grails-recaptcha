a36b7f07f1c7bef7b52e559a86939d8f3e7cf186
Chad Johnston <cjohnston@megatome.com>
2010-07-24 02:22:53 +0000

    Update description

--- RecaptchaGrailsPlugin.groovy
+++ RecaptchaGrailsPlugin.groovy
@@ -4,7 +4,7 @@ class RecaptchaGrailsPlugin {
     def grailsVersion = "1.1 > *"
     def author = "Chad Johnston"
     def authorEmail = "cjohnston@megatome.com"
-    def title = "This plugin adds ReCaptcha support to Grails."
+    def title = "This plugin adds ReCaptcha and Mailhide support to Grails."
     def description = '''\
 ReCaptcha is a CAPTCHA implementation that has a goal of better digitizing books by having users identify words that traditional OCR systems have missed.
 This plugin uses the ReCaptcha Java library available from http://code.google.com/p/recaptcha/.
