84855210ad0925071c619101f9a08c12271f7ddb
Chad Johnston <cjohnston@megatome.com>
2013-03-04 18:54:27 -0700

    Finish quickstart script.

--- scripts/_Install.groovy
+++ scripts/_Install.groovy
@@ -1,17 +1,9 @@
-
-//
-// This script is executed by Grails after plugin was installed to project.
-// This script is a Gant script so you can use all special variables provided
-// by Gant (such as 'baseDir' which points on project base dir). You can
-// use 'Ant' to access a global instance of AntBuilder
-//
-//
-if (!ant.available(file: "${basedir}/grails-app/conf/RecaptchaConfig.groovy")) {
-
-    ant.copy(file:"${pluginBasedir}/src/templates/RecaptchaConfig.groovy",
-             todir:"${basedir}/grails-app/conf")
-}
-
-ant.property(environment:"env")
-grailsHome = ant.antProject.properties."env.GRAILS_HOME"
-
+println '''
+*******************************************************
+* You've installed the Recaptcha plugin.              *
+*                                                     *
+* Next run the "recaptcha-quickstart" script to       *
+* initialize the configuration.                       *
+*                                                     *
+*******************************************************
+'''
