5c29760391b36f9b9d5299e92a044005168d5259
Chad Johnston <cjohnston@megatome.com>
2015-08-21 14:48:37 -0600

    Add more tests. Refactor existing.

--- test/unit/com/megatome/grails/RecaptchaTagLibTest.groovy
+++ test/unit/com/megatome/grails/RecaptchaTagLibTest.groovy
@@ -5,28 +5,48 @@ import spock.lang.Specification
 
 @TestFor(RecaptchaTagLib)
 class RecaptchaTagLibTest extends Specification {
-    void "test recaptcha tag with no attributes"() {
-        given:
-        def recapMock = mockFor(RecaptchaService)
-        def params
-        recapMock.demand.createCaptcha { Map props -> params = props; return "" }
-        tagLib.recaptchaService = recapMock.createMock()
+    def recapMock
+    def params
 
-        expect:
-        tagLib.recaptcha() == ""
+    /*def setup() {
+        recapMock = mockFor(RecaptchaService)
+        params = [:]
+        recapMock.demand.createCaptcha { Map props -> params = props; return "" }
+        recapMock.demand.createCaptchaExplicit { Map props -> params = props; return "" }
+        tagLib.recaptchaService = recapMock.createMock()
+    }*/
+
+    def setupMock(explicit) {
+        recapMock = mockFor(RecaptchaService)
+        params = [:]
+        if (!explicit) {
+            recapMock.demand.createCaptcha { Map props -> params = props; return "" }
+        } else {
+            recapMock.demand.createCaptchaExplicit { Map props -> params = props; return "" }
+        }
+        tagLib.recaptchaService = recapMock.createMock()
+    }
+
+    void "test recaptcha tag with no attributes"() {
+        setup:
+        setupMock(false)
+
+        when:
+        tagLib.recaptcha()
+
+        then:
         params != null
         params == [:]
     }
 
     void "test recaptcha tag with all attributes"() {
-        given:
-        def recapMock = mockFor(RecaptchaService)
-        def params
-        recapMock.demand.createCaptcha { Map props -> params = props; return "" }
-        tagLib.recaptchaService = recapMock.createMock()
+        setup:
+        setupMock(false)
 
-        expect:
-        tagLib.recaptcha(theme:"dark", lang:"fr", successCallback: "successCB", expiredCallback: "expiredCB", tabindex: 1, includeScript: true) == ""
+        when:
+        tagLib.recaptcha(theme:"dark", lang:"fr", successCallback: "successCB", expiredCallback: "expiredCB", tabindex: 1, includeScript: true)
+
+        then:
         params != null
         params.theme == "dark"
         params.lang == "fr"
@@ -37,16 +57,53 @@ class RecaptchaTagLibTest extends Specification {
     }
 
     void "test recaptcha tag with illegal attributes"() {
-        given:
-        def recapMock = mockFor(RecaptchaService)
-        def params
-        recapMock.demand.createCaptcha { Map props -> params = props; return "" }
-        tagLib.recaptchaService = recapMock.createMock()
+        setup:
+        setupMock(false)
 
-        expect:
-        tagLib.recaptcha(theme:"dark", foo:"bar") == ""
+        when:
+        tagLib.recaptcha(theme:"dark", foo:"bar")
+
+        then:
         params != null
         params.theme == "dark"
+        !params.containsKey('foo')
+    }
+
+    void "test recaptchaExplicit tag with no attributes"() {
+        setup:
+        setupMock(true)
+
+        when:
+        tagLib.recaptchaExplicit()
+
+        then:
+        params != null
+        params == [:]
+    }
+
+    void "test recaptchaExplicit tag with all attributes"() {
+        setup:
+        setupMock(true)
+
+        when:
+        tagLib.recaptchaExplicit(lang:"fr", loadCallback: "loadCB")
+
+        then:
+        params != null
+        params.lang == "fr"
+        params.loadCallback == "loadCB"
+    }
+
+    void "test recaptchaExplicit tag with illegal attributes"() {
+        setup:
+        setupMock(true)
+
+        when:
+        tagLib.recaptchaExplicit(lang:"fr", foo:"bar")
+
+        then:
+        params != null
+        params.lang == "fr"
         !params.containsKey('foo')
     }
 }
