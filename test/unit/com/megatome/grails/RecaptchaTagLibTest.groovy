15693bf6a1473517f513ed110aee81242bdce35b
Chad Johnston <cjohnston@megatome.com>
2015-08-21 20:40:41 -0600

    More and more tests

--- test/unit/com/megatome/grails/RecaptchaTagLibTest.groovy
+++ test/unit/com/megatome/grails/RecaptchaTagLibTest.groovy
@@ -5,7 +5,7 @@ import spock.lang.Specification
 
 @TestFor(RecaptchaTagLib)
 class RecaptchaTagLibTest extends Specification {
-    void "test recaptcha tag with no attributes"() {
+    void "test recaptcha tag"() {
         setup:
         def recapMock = mockFor(RecaptchaService)
         def params = [:]
@@ -41,7 +41,7 @@ class RecaptchaTagLibTest extends Specification {
         !params.containsKey('foo')
     }
 
-    void "test recaptchaExplicit tag with no attributes"() {
+    void "test recaptchaExplicit tag"() {
         setup:
         def recapMock = mockFor(RecaptchaService)
         def params = [:]
@@ -72,7 +72,7 @@ class RecaptchaTagLibTest extends Specification {
         !params.containsKey('foo')
     }
 
-    void "test renderParametrs tag"() {
+    void "test renderParameters tag"() {
         setup:
         def recapMock = mockFor(RecaptchaService)
         def params = [:]
@@ -104,5 +104,144 @@ class RecaptchaTagLibTest extends Specification {
         params != null
         params.theme == "dark"
         !params.containsKey('foo')
+    }
+
+    void "test script tag"() {
+        setup:
+        def recapMock = mockFor(RecaptchaService)
+        def params = [:]
+        recapMock.demand.createScriptEntry(3..3) { Map props -> params = props; return "" }
+        tagLib.recaptchaService = recapMock.createMock()
+
+        when:
+        tagLib.script()
+
+        then:
+        params != null
+        params == [:]
+
+        when:
+        tagLib.script(lang:"fr")
+
+        then:
+        params != null
+        params.lang == "fr"
+
+        when:
+        tagLib.script(lang:"fr", foo:"bar")
+
+        then:
+        params != null
+        params.lang == "fr"
+        !params.containsKey('foo')
+    }
+
+    void "test ifEnabled(true)/isDisabled(false) tag"() {
+        setup:
+        def recapMock = mockFor(RecaptchaService)
+        recapMock.demand.isEnabled(2..2) { -> true }
+        tagLib.recaptchaService = recapMock.createMock()
+
+        when:
+        def response = tagLib.ifEnabled(null, { "Enabled" })
+
+        then:
+        response.toString() == "Enabled"
+
+        when:
+        response = tagLib.ifDisabled(null, { "Disabled" })
+
+        then:
+        response.toString() == ""
+    }
+
+    void "test ifEnabled(false)/isDisabled(true) tag"() {
+        setup:
+        def recapMock = mockFor(RecaptchaService)
+        recapMock.demand.isEnabled(2..2) { -> false }
+        tagLib.recaptchaService = recapMock.createMock()
+
+        when:
+        def response = tagLib.ifEnabled(null, { "Enabled" })
+
+        then:
+        response.toString() == ""
+
+        when:
+        response = tagLib.ifDisabled(null, { "Disabled" })
+
+        then:
+        response.toString() == "Disabled"
+    }
+
+    void "test ifFailed(true) tag"() {
+        setup:
+        def recapMock = mockFor(RecaptchaService)
+        recapMock.demand.validationFailed { session -> true }
+        tagLib.recaptchaService = recapMock.createMock()
+
+        when:
+        def response = tagLib.ifFailed(null, { "Failed" })
+
+        then:
+        response.toString() == "Failed"
+    }
+
+    void "test ifFailed(false) tag"() {
+        setup:
+        def recapMock = mockFor(RecaptchaService)
+        recapMock.demand.validationFailed { session -> false }
+        tagLib.recaptchaService = recapMock.createMock()
+
+        when:
+        def response = tagLib.ifFailed(null, { "Success" })
+
+        then:
+        response.toString() == ""
+    }
+
+    void "test mailhide tag"() {
+        setup:
+        def mailhideMock = mockFor(MailhideService)
+        mailhideMock.demand.createMailhideURL(2..2) { -> "http://fake.url.com" }
+        tagLib.mailhideService = mailhideMock.createMock()
+
+        when:
+        tagLib.mailhide()
+
+        then:
+        thrown(IllegalArgumentException)
+
+        when:
+        def response = tagLib.mailhide(emailAddress: 'a@b.com', { "Link wrap text" }).toString()
+
+        then:
+        response.contains("http://fake.url.com")
+        response.contains("Link wrap text")
+    }
+
+    void "test mailhideURL tag"() {
+        setup:
+        def mailhideMock = mockFor(MailhideService)
+        mailhideMock.demand.createMailhideURL(3..3) { -> "http://fake.url.com" }
+        tagLib.mailhideService = mailhideMock.createMock()
+
+        when:
+        tagLib.mailhideURL()
+
+        then:
+        thrown(IllegalArgumentException)
+
+        when:
+        def response = tagLib.mailhideURL(emailAddress: 'a@b.com', { var -> var })
+
+        then:
+        response.contains("mailhideURL:")
+
+        when:
+        response = tagLib.mailhideURL([emailAddress: 'a@b.com', var: 'foo'], { var -> var })
+
+        then:
+        response.contains("foo:")
     }
 }
